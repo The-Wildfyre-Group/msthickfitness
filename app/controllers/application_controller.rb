2b3cc5ae5990beadeab48ac5bc4da110e31198f4
Cordis Stanfield <cordis.stanfield@gmail.com>
2014-07-10 16:48:03 -0400

    Add http authenticate method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  def http_basic_authenticate
+    if Rails.env.production?
+      authenticate_or_request_with_http_basic do |username, password|
+        username == "spa" && password == "spa"
+      end
+    else
+      true
+    end
+  end
 end
